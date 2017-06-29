e22baa069eb8642e06bb4263d5a5d80fd042492b
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-28 23:49:36 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', port);
+
+app.listen(app.get('port'), () => console.log("Listening on port: " + app.get("port")));
